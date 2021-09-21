6e185147885d273ca846ef52510def9fcf0db10f
jeremiasp96 <palaciostoconasomarjeremias@gmail.com>
2021-09-21 16:16:19 -0300

    static

--- social_django/settings.py
+++ social_django/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
