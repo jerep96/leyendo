7752c5ff229ba1bd99dd4aadf5cb0ea68782e700
jeremiasp96 <palaciostoconasomarjeremias@gmail.com>
2021-09-21 10:06:03 -0300

    archivos de cofiguracion

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn social_django.wsgi --log-file -
---
+++ gunicorn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /var/www/leyendo
+source venv/bin/activate
+gunicorn social_django.wsgi -t 600 -b 0.0.0.0:8000 -w 6 --user=root --log-file=/var/www/leyendo/gunicorn.log
