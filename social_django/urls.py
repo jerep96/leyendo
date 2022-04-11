11f940926e5f366f188f46590352f2b23488ab87
jerep96 <jeremiasp96@gmail.com>
2022-04-11 18:07:41 -0300

    static

--- social_django/urls.py
+++ social_django/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from social_django import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('social.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
